e5ade195d41a148961d113493fa5a9635fe73c5d
Jan Eberhardt <eberhardt@tu-berlin.de>
2020-03-28 16:40:35 +0100

    cleaner code
    Changes to be committed:
    	modified:   livestreaming/streams.py

--- livestreaming/streams.py
+++ livestreaming/streams.py
@@ -7,10 +7,10 @@ from enum import Enum
 
 class StreamState(Enum):
     NOT_YET_STARTED = 0
-    WAITING_FOR_CONNECTION = 1 # streamer did not yet connect to open port
-    BUFFERING = 2 # streamer connected, but waiting now until there is enough data (first segments have been written)
-    STREAMING = 3 # streaming started, content node may fetch data from encoder and clients may connect to content node
-    STOPPED = 4 # streamer stopped broadcasting, wait until manager tells to clean up
+    WAITING_FOR_CONNECTION = 1  # streamer did not yet connect to open port
+    BUFFERING = 2  # streamer connected, but waiting now until there is enough data (first segments have been written)
+    STREAMING = 3  # streaming started, content node may fetch data from encoder and clients may connect to content node
+    STOPPED = 4  # streamer stopped broadcasting, wait until manager tells to clean up
     ERROR = 100
     UNEXPECTED_STREAM = 104
     UNKNOWN = 200
