03edb5416e6af642bb0289f1669caf9eda1c4c7c
Daniil Fajnberg <mail@daniil.fajnberg.de>
2020-08-10 15:38:07 +0200

    refactored DNSManager and subclass
    _update_record method and slightly rewritten _add_record
    
    slightly rewritten _get_all_records
    
    in _add_record method: first check existing records, then either update or add new
    
    new methods: create_a_record and create_aaaa_record
    
    PEP8 stuff
    
    PEP8 stuff
    
    remove_all_records method
    
    generic ip address type

--- videbo/manager/cloud/dns_api.py
+++ videbo/manager/cloud/dns_api.py
@@ -2,21 +2,23 @@ import INWX
 from asyncio import get_event_loop
 from contextlib import contextmanager
 from dataclasses import dataclass
-from ipaddress import IPv4Address
+from ipaddress import IPv4Address, IPv6Address
 import logging
 from threading import Lock
-from typing import List, Optional
+from typing import List, Optional, Union
 from videbo.manager import manager_settings
 from .definitions import INWXApiAuthDefinition, CloudInstanceDefsController
 
+
+_GenericIPAddress = Union[IPv4Address, IPv6Address]
 
 logger = logging.getLogger('videbo-dns')
 
 
 @dataclass
 class DNSRecord:
-    type: str # A, AAAA, MX, ...
-    name: str # including full domain
+    type: str  # A, AAAA, MX, ...
+    name: str  # including full domain
     content: str
     ttl: int = 3600
     prio: int = 0
@@ -73,6 +75,40 @@ class DNSManager:
         """Internal method that needs to be overridden by dns provider api."""
         raise NotImplementedError
 
+    async def create_a_record(self, subdomain: str, ip: IPv4Address) -> DNSRecord:
+        return await self._create_record(subdomain, ip, 'A')
+
+    async def create_aaaa_record(self, subdomain: str, ip: IPv6Address) -> DNSRecord:
+        return await self._create_record(subdomain, ip, 'AAAA')
+
+    async def _create_record(self, subdomain: str, ip: _GenericIPAddress, rec_type: str) -> DNSRecord:
+        """
+        Checks `cached_records` for a record with `matching` name and `type`.
+        If found and `content` matches, returns that record.
+        If `content` doesn't match, the record's `content` is changed and `add_record` is called.
+        (Assumes that `add_record` does an update on existing records.)
+        If no match is found at all, a new record is initialized and `add_record` is called.
+        """
+        rec = None
+        rec_name = f'{manager_settings.dynamic_node_name_prefix}{subdomain}.{self.domain}'
+        for record in self.cached_records:
+            if record.name == rec_name and record.type == rec_type:
+                if record.content == str(ip):
+                    return record
+                rec = record
+                rec.content = str(ip)
+                break
+        if rec is None:
+            rec = DNSRecord(type=rec_type, name=rec_name, content=str(ip))
+        await self.add_record(rec)
+        return rec
+
+    async def remove_all_records(self, subdomain: str) -> None:
+        rec_name = f'{manager_settings.dynamic_node_name_prefix}{subdomain}.{self.domain}'
+        for record in self.cached_records:
+            if record.name == rec_name:
+                await self.remove_record(record)
+
     async def add_dynamic_node_name_from_ipv4(self, ip: IPv4Address) -> DNSRecord:
         """Add new record for a dynamic node and return the full host name."""
         new_record = DNSRecord.create_dynamic_node_name_from_ipv4(self.domain, ip)
@@ -87,11 +123,15 @@ class DNSManager:
 
 
 class DNSManagerINWX(DNSManager):
+    """
+    Documentation - return codes:
+    https://www.inwx.de/en/help/apidoc/f/ch04.html
+    """
     def __init__(self, domain: str, auth: INWXApiAuthDefinition):
         super().__init__(domain)
         self.username: str = auth.username
         self.password: str = auth.password
-        self.lock = Lock() # only one request at the same time allowed
+        self.lock = Lock()  # only one request at the same time allowed
 
     @contextmanager
     def context_manager_session(self):
@@ -105,7 +145,7 @@ class DNSManagerINWX(DNSManager):
 
             try:
                 yield api
-            except:
+            except Exception:
                 raise DNSAPIError()
             finally:
                 try:
@@ -117,55 +157,84 @@ class DNSManagerINWX(DNSManager):
         return await get_event_loop().run_in_executor(None, self._get_all_records_sync)
 
     def _get_all_records_sync(self) -> List[DNSRecord]:
-        try:
-            found_records: List[DNSRecord] = []
-
-            api: INWX.ApiClient
-            with self.context_manager_session() as api:
-                params = {
-                    'domain': self.domain,
-                }
+        """
+        Documentation - method:
+        https://www.inwx.de/en/help/apidoc/f/ch02s13.html#nameserver.info
+        """
+        found_records: List[DNSRecord] = []
+        params = {'domain': self.domain}
+        with self.context_manager_session() as api:
+            try:
                 ret: dict = api.call_api(api_method="nameserver.info", method_params=params)
-
-                if ret['code'] == 1000 and ret['resData']['domain'] == self.domain:
-                    for record in ret['resData']['record']:
-                        new_record = DNSRecord(type=record['type'], name=record['name'], content=record['content'],
-                                               ttl=record['ttl'], prio=record['prio'], id=record['id'])
-                        found_records.append(new_record)
-
-                    return found_records
-
+            except Exception:
                 raise DNSAPIError()
-        except:
+            if ret['code'] == 1000 and ret['resData']['domain'] == self.domain:
+                for record in ret['resData']['record']:
+                    new_record = DNSRecord(type=record['type'], name=record['name'], content=record['content'],
+                                           ttl=record['ttl'], prio=record['prio'], id=record['id'])
+                    found_records.append(new_record)
+
+                return found_records
             raise DNSAPIError()
 
     async def _add_record(self, record: DNSRecord):
-        await get_event_loop().run_in_executor(None, self._add_record_sync, record)
+        """
+        First fetches all existing records.
+        If an existing record with the requested `type` and `name` exists, the record is updated.
+        Otherwise, a new record is added.
+        """
+        existing_records = await get_event_loop().run_in_executor(None, self._get_all_records_sync)
+        for ex_rec in existing_records:
+            if ex_rec.type == record.type and ex_rec.name == record.name:
+                return await self._update_record(record)
+        get_event_loop().run_in_executor(None, self._add_record_sync, record)
 
-    def _add_record_sync(self, record: DNSRecord):
-        try:
-            api: INWX.ApiClient
-            with self.context_manager_session() as api:
-                params = {
-                    'domain': self.domain,
-                    'type': record.type,
-                    'content': record.content,
-                    'name': record.name,
-                    'ttl': record.ttl,
-                    'prio': record.prio,
-                }
+    def _add_record_sync(self, record: DNSRecord) -> None:
+        """
+        Documentation - method:
+        https://www.inwx.de/en/help/apidoc/f/ch02s13.html#nameserver.createRecord
+        """
+        params = {
+            'domain': self.domain,
+            'type': record.type,
+            'content': record.content,
+            'name': record.name,
+            'ttl': record.ttl,
+            'prio': record.prio,
+        }
+        with self.context_manager_session() as api:
+            try:
                 ret: dict = api.call_api(api_method="nameserver.createRecord", method_params=params)
+            except Exception:
+                raise DNSAPIError()
+            if ret['code'] == 1000:
+                record.id = ret['resData']['id']
+            else:
+                raise DNSAPIError()
 
-                if ret['code'] == 1000:
-                    record.id = ret['resData']['id']
-                elif ret['code'] == 2302:
-                    # record already exists
-                    # TODO: update record instead of creating a new one
-                    raise DNSAPIError()
-                else:
-                    raise DNSAPIError()
-        except:
-            raise DNSAPIError()
+    async def _update_record(self, record: DNSRecord):
+        await get_event_loop().run_in_executor(None, self._update_record_sync, record)
+
+    def _update_record_sync(self, record: DNSRecord):
+        """
+        Documentation - method:
+        https://www.inwx.de/en/help/apidoc/f/ch02s13.html#nameserver.updateRecord
+        """
+        params = {
+            'id': record.id,
+            'type': record.type,
+            'name': record.name,
+            'content': record.content,
+            'ttl': record.ttl,
+            'prio': record.prio,
+        }
+        with self.context_manager_session() as api:
+            try:
+                ret: dict = api.call_api(api_method="nameserver.updateRecord", method_params=params)
+            except Exception:
+                raise DNSAPIError()
+            if ret['code'] != 1000:
+                raise DNSAPIError()
 
     async def _remove_record(self, record: DNSRecord):
         await get_event_loop().run_in_executor(None, self._remove_record_sync, record)
@@ -185,7 +254,7 @@ class DNSManagerINWX(DNSManager):
                     return
                 else:
                     raise DNSAPIError()
-        except:
+        except Exception:
             raise DNSAPIError()
 
 
