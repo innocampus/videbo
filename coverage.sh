a41ca210d2c66e815a27621673daa7c770f3babf
Daniil Fajnberg <mail@daniil.fajnberg.de>
2022-09-01 18:18:13 +0200

    ⚙ Add Github workflow for tests and type checks; add `tests` entry point script; improve `coverage.sh` script

--- coverage.sh
+++ coverage.sh
@@ -1,3 +1,10 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-coverage erase && coverage run -m unittest discover && coverage report
+coverage erase
+# Capture the test progression in a variable:
+typeset progress
+progress=$(coverage run 2>&1)
+# If tests failed or produced errors, write progress/messages to stderr and exit:
+[[ $? -eq 0 ]] || { >&2 echo "${progress}"; exit 1; }
+# Otherwise extract the total coverage percentage from the produced report and write it to stdout:
+coverage report | awk '$1 == "TOTAL" {print $NF; exit}'
