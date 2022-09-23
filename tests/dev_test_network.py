981e3e154d951836f91f2b5d1bd51854a937182f
Daniil Fajnberg <mail@daniil.fajnberg.de>
2022-09-23 14:43:00 +0200

    ✅ Add simple dev test script for network interface info fetching

---
+++ tests/dev_test_network.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""
+Simple script to test network interface information fetching.
+"""
+
+import asyncio
+import logging
+import sys
+
+from videbo import storage_settings
+from videbo.network import NetworkInterfaces
+
+
+async def main():
+    ni = NetworkInterfaces.get_instance()
+    log = logging.getLogger()
+    log.setLevel(logging.DEBUG)
+    log.addHandler(logging.StreamHandler(sys.stdout))
+    ni.start_fetching(storage_settings)
+    while True:
+        print(f"Fetching: {ni.is_fetching}")
+        print(repr(ni.get_first_interface() or "No interface"))
+        await asyncio.sleep(3)
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("\rStopped.")
