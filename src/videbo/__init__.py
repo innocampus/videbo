509d02040d9a44e168d79a4e75817c7ccafdad2d
Daniil Fajnberg <d.fajnberg@tu-berlin.de>
2024-07-25 18:01:54 +0200

    🔖 Release v0.7.0

--- src/videbo/__init__.py
+++ src/videbo/__init__.py
@@ -14,7 +14,7 @@ See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with videbo.
 If not, see <https://www.gnu.org/licenses/>."""
 
-__version__ = "0.6.0"
+__version__ = "0.7.0"
 
 __doc__ = """
 Top-level global settings object for convenient import.
